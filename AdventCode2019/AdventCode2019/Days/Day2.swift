f2eb35229467179f1e0a99f578cd8e762092cf1f
Arnaud Le Bourblanc <arnaud.lebourblanc@gmail.com>
2019-12-03 23:33:07 +0100

    Added day2 resolution

--- AdventCode2019/AdventCode2019/Days/Day2.swift
+++ AdventCode2019/AdventCode2019/Days/Day2.swift
@@ -13,8 +13,59 @@ class Day2: Day {
 
     override var status: DayStatus { return .notDone }
 
-    override func getResult() -> String {
+    override func loadInput() {
+        guard let data = getDataInput(),
+            let str = String(data: data, encoding: .utf8) else {
+            return
+        }
+        let elements = str.components(separatedBy: ",")
+        self.input = elements.compactMap({Int($0)})
+    }
 
+    override func getResult() -> String {
+        let input: [Int]! = self.input
+        for i in 0...99 {
+            for j in 0...99 {
+                if get(noun: i, verb: j, input: input) == 19690720 {
+                    return "\(100 * i + j)"
+                }
+            }
+        }
         return ""
+    }
+
+    private func get(noun: Int, verb: Int, input: [Int]) -> Int {
+        var input = input
+        var index = 0
+        input[1] = noun
+        input[2] = verb
+        while let op = Operator(rawValue: input[index]), op != .abort {
+            let leftIndex = input[index+1]
+            let rightIndex = input[index+2]
+            let replacementIndex = input[index+3]
+            let leftValue = input[leftIndex]
+            let rightValue = input[rightIndex]
+            let value = op.operate(leftValue: leftValue, rightValue: rightValue)
+            input[replacementIndex] = value
+            index+=4
+        }
+        return input[0]
+    }
+
+    private enum Operator: Int {
+        case add = 1
+        case multiply = 2
+        case abort = 99
+
+        func operate(leftValue: Int, rightValue: Int) -> Int {
+            switch self {
+            case .add:
+                return leftValue + rightValue
+            case .multiply:
+                return leftValue * rightValue
+            default:
+                return -1
+            }
+        }
     }
 }
